fe9ccbb8f81aafa3b658a8d19cde4d218f252500
5881 <duospirit@gmail.com>
2022-06-19 17:21:15 +0300

    v2.0

---
+++ calc_dg.py
@@ -0,0 +1,268 @@
+#!/bin/python
+#Скрипт для рассчёта dG образования веществ в растворе
+#Скрипт принимает на вход файлы xyz, проводит предварительную оптимизацию
+#PBEH-3c затем B3LYP(D3BJ)/def2-tzvp, считает гессиан
+#затем считается single point для полученой структуры методами
+#RI-MP2/ma-def2-tzvp и dlpno-ccsd(t)/ma-def2-tzvp
+#Полученые данные заносятся в файл
+#перед использованием следует задать правильные значения переменных
+#ORCA,SOLVENT,MAXCORE,NPROC,MAXCORE_DLPNOCCSDT,NPROC_DLPNOCCSDT
+#Скрипт проверен с ORCA 5.0.3
+import os,sys
+ORCA=os.path.expanduser('~')+"/orca/orca"
+REPORTFILE="energy_report_file.csv"
+SOLVENT="DMSO"
+MAXCORE="15000"
+NPROC="16"
+#Для DLPNO выставляется отдельно тк метод капризный
+MAXCORE_DLPNOCCSDT="16000"
+NPROC_DLPNOCCSDT="15"
+
+def output_singlepoint_analyse(outputfile):
+	E=0
+	with open(outputfile, 'r') as out_file:
+		for s in out_file:
+			if 'FINAL SINGLE POINT ENERGY' in s: E=float(s.split()[-1])
+	print(outputfile, "E=", E)
+	#print("eee",E+G_el)
+	return E
+
+def output_freq_analyse(outputfile):
+	gibbs_done=0 
+	vib_freq_done=0
+	geom_ok=1
+	G=0
+	G_el=0
+	E=0
+	with open(outputfile, 'r') as out_file:
+		for s in out_file:
+			if 'FINAL SINGLE POINT ENERGY' in s: E=float(s.split()[-1])
+			if 'VIBRATIONAL FREQUENCIES' in s: vib_freq_done=1
+			if vib_freq_done and '***imaginary mode***' in s:
+				print("Мнимые частоты, геометрия возможно неверна")
+				geom_ok=0
+			if 'GIBBS FREE ENERGY' in s : 
+				gibbs_done=1
+				vib_freq_done=0
+			if gibbs_done and 'Final Gibbs free energy' in s:
+				G=float(s.split()[-2])
+			if gibbs_done and 'G-E(el)' in s:
+				G_el=float(s.split()[-4])
+	print(outputfile, "E=", E, "G=", G,"G-el=",G_el, "OK=",geom_ok)
+	#print("eee",E+G_el)
+	return E, G, G_el, geom_ok
+
+def getxyz_from_file(filename):
+	xyz=[]
+	with open(filename, 'r', encoding="utf-8", errors="ignore") as xyz_file:
+		for line in xyz_file.readlines():
+			if (len(line.split()) == 4 and line.split()[-1][-1].isdigit()):
+				xyz.append(line[:-1])
+	return xyz
+
+def copyfile(src, dst):
+	cmd="cp "+src+" "+dst
+	os.system(cmd)
+	
+def pbeh3c_opt(xyzfile):
+	job="_pbeh3c_opt"
+	basename=xyzfile.replace('.xyz', '').rsplit('_')[0]
+	basename=basename+job
+	dirname=basename
+	inpfile=basename+".inp"
+	outputfile=basename+".out"
+	xyzoutputfile=basename+".xyz"
+	charge=0
+	xyz=getxyz_from_file(xyzfile)
+	if(basename.find("-")!=-1): charge=-1
+	if(basename.find("+")!=-1): charge=1
+	if(os.path.isdir(dirname) is False):
+		os.mkdir(dirname)
+	os.chdir(dirname)
+	if(os.path.isfile(outputfile) is False):
+		inputfilecontent="""
+%pal nprocs NPROC end
+%maxcore MAXCORE
+!pbeh-3c opt cpcm(SOLVENT) xyzfile
+%geom MaxIter 500 end
+"""
+		inputfilecontent=inputfilecontent.replace('NPROC',NPROC)
+		inputfilecontent=inputfilecontent.replace('MAXCORE',MAXCORE)
+		inputfilecontent=inputfilecontent.replace('SOLVENT',SOLVENT)
+		with open(inpfile, 'w') as inp_file:
+			print(inputfilecontent,file=inp_file)
+			print("* xyz", charge,"1",file=inp_file)
+			for s in xyz:
+				print(s,file=inp_file)
+			print("*",file=inp_file)
+		orcacmd=ORCA+" "+inpfile+"|tee "+outputfile
+		os.system(orcacmd)
+		os.chdir("..")
+		outputfile=dirname+"/"+outputfile
+	else:
+		os.chdir("..")
+	xyzfile=dirname+"/"+xyzoutputfile
+	return xyzfile
+
+def b3lyp_d3bj_finalopt_freq_solv(xyzfile):
+	job="_b3lyp_d3bj_tzvp_optfreq"
+	basename=xyzfile.replace('.xyz', '').rsplit('_')[0]
+	basename=basename+job
+	dirname=basename
+	inpfile=basename+".inp"
+	outputfile=basename+".out"
+	xyzoutputfile=basename+".xyz"
+	charge=0
+	xyz=getxyz_from_file(xyzfile)
+	if(basename.find("-")!=-1): charge=-1
+	if(basename.find("+")!=-1): charge=1
+	if(os.path.isdir(dirname) is False):
+		os.mkdir(dirname)
+	os.chdir(dirname)
+	if(os.path.isfile(outputfile) is False):
+		inputfilecontent="""
+%pal nprocs NPROC end
+%maxcore MAXCORE
+!b3lyp d3bj def2-tzvp def2-tzvp/c def2/j rijcosx opt freq tightscf tightopt cpcm(SOLVENT) xyzfile
+%geom MaxIter 500 end
+"""
+		inputfilecontent=inputfilecontent.replace('NPROC',NPROC)
+		inputfilecontent=inputfilecontent.replace('MAXCORE',MAXCORE)
+		inputfilecontent=inputfilecontent.replace('SOLVENT',SOLVENT)
+		with open(inpfile, 'w') as inp_file:
+			print(inputfilecontent,file=inp_file)
+			print("* xyz", charge,"1",file=inp_file)
+			for s in xyz:
+				print(s,file=inp_file)
+			print("*",file=inp_file)
+		orcacmd=ORCA+" "+inpfile+"|tee "+outputfile
+		os.system(orcacmd)
+		os.chdir("..")
+	else:
+		os.chdir("..")
+	xyzfile=dirname+"/"+xyzoutputfile
+	outputfile=dirname+"/"+outputfile
+	return outputfile,xyzfile
+
+def ri_mp2_singlepoint_solv(xyzfile):
+	job="_rimp2_matzvp_solv"
+	basename=xyzfile.replace('.xyz', '').rsplit('_')[0]
+	basename=basename+job
+	dirname=basename
+	inpfile=basename+".inp"
+	outputfile=basename+".out"
+	xyzoutputfile=basename+".xyz"
+	charge=0
+	xyz=getxyz_from_file(xyzfile)
+	if(basename.find("-")!=-1): charge=-1
+	if(basename.find("+")!=-1): charge=1
+	if(os.path.isdir(dirname) is False):
+		os.mkdir(dirname)
+	os.chdir(dirname)
+	if(os.path.isfile(outputfile) is False):
+		inputfilecontent="""
+%pal nprocs NPROC end
+%maxcore MAXCORE
+!RI-MP2 ma-def2-tzvp autoaux tightscf nofrozencore cpcm(SOLVENT)
+%basis
+PCDTrimBas Overlap # Trim the orbital basis in the overlap metric
+PCDTrimAuxJ Coulomb # Trim the AuxJ basis in the Coulomb metric
+PCDTrimAuxJK Coulomb # Trim the AuxJK basis in the Coulomb metric
+PCDTrimAuxC Coulomb # Trim the AuxC basis in the Coulomb metric
+PCDThresh -1 # Threshold for the PCD: chosen automatically if <0
+end
+%scf
+Thresh 1e-12
+Sthresh 1e-7
+end
+"""
+		inputfilecontent=inputfilecontent.replace('NPROC',NPROC)
+		inputfilecontent=inputfilecontent.replace('MAXCORE',MAXCORE)
+		inputfilecontent=inputfilecontent.replace('SOLVENT',SOLVENT)
+		with open(inpfile, 'w') as inp_file:
+			print(inputfilecontent,file=inp_file)
+			print("* xyz", charge,"1",file=inp_file)
+			for s in xyz:
+				print(s,file=inp_file)
+			print("*",file=inp_file)
+		orcacmd=ORCA+" "+inpfile+"|tee "+outputfile
+		os.system(orcacmd)
+		os.chdir("..")
+	else:
+		os.chdir("..")
+	#xyzfile=dirname+"/"+xyzoutputfile
+	outputfile=dirname+"/"+outputfile
+	return outputfile
+
+def dlpno_ccsdt_singlepoint_solv(xyzfile):
+	job="_dlpnoccsdt_matzvp_solv"
+	basename=xyzfile.replace('.xyz', '').rsplit('_')[0]
+	basename=basename+job
+	dirname=basename
+	inpfile=basename+".inp"
+	outputfile=basename+".out"
+	xyzoutputfile=basename+".xyz"
+	charge=0
+	xyz=getxyz_from_file(xyzfile)
+	if(basename.find("-")!=-1): charge=-1
+	if(basename.find("+")!=-1): charge=1
+	if(os.path.isdir(dirname) is False):
+		os.mkdir(dirname)
+	os.chdir(dirname)
+	if(os.path.isfile(outputfile) is False):
+		inputfilecontent="""
+%pal nprocs NPROC end
+%maxcore MAXCORE
+!dlpno-ccsd(t) ma-def2-tzvp autoaux tightscf cpcm(SOLVENT)
+%basis
+PCDTrimBas Overlap # Trim the orbital basis in the overlap metric
+PCDTrimAuxJ Coulomb # Trim the AuxJ basis in the Coulomb metric
+PCDTrimAuxJK Coulomb # Trim the AuxJK basis in the Coulomb metric
+PCDTrimAuxC Coulomb # Trim the AuxC basis in the Coulomb metric
+PCDThresh -1 # Threshold for the PCD: chosen automatically if <0
+end
+%scf
+Thresh 1e-12
+Sthresh 1e-7
+end
+"""
+		inputfilecontent=inputfilecontent.replace('NPROC',NPROC_DLPNOCCSDT)
+		inputfilecontent=inputfilecontent.replace('MAXCORE',MAXCORE_DLPNOCCSDT)
+		inputfilecontent=inputfilecontent.replace('SOLVENT',SOLVENT)
+		with open(inpfile, 'w') as inp_file:
+			print(inputfilecontent,file=inp_file)
+			print("* xyz", charge,"1",file=inp_file)
+			for s in xyz:
+				print(s,file=inp_file)
+			print("*",file=inp_file)
+		orcacmd=ORCA+" "+inpfile+"|tee "+outputfile
+		os.system(orcacmd)
+		os.chdir("..")
+	else:
+		os.chdir("..")
+	#xyzfile=dirname+"/"+xyzoutputfile
+	outputfile=dirname+"/"+outputfile
+	return outputfile
+
+
+#создаём файл отчёта если его нет, вносим данные для DMSO чтобы сэкономить время
+if(os.path.isfile(REPORTFILE) is False):
+	with open(REPORTFILE, 'w') as reportfile:
+				print(
+"""xyzfile,geom OK, G, G-Eel, E_RI-MP2, E_DLPNO-CCSD(T)
+dmso-_b3lyp_d3bj_tzvp_optfreq.xyz, 1, -552.60891359, 0.03723523, -552.067138692847, -551.97374571165
+dmso_b3lyp_d3bj_tzvp_optfreq.xyz, 1, -553.12829837, 0.0508621, -552.59076969229, -552.502786965165"""
+,file=reportfile)
+
+
+for xyzinputfile in sys.argv[1:]:
+	stepxyzfile=pbeh3c_opt(xyzinputfile)
+	stepoutputfile,stepxyzfile=b3lyp_d3bj_finalopt_freq_solv(stepxyzfile)
+	E, G, G_el, geom_ok=output_freq_analyse(stepoutputfile)
+	stepoutputfile=ri_mp2_singlepoint_solv(stepxyzfile)
+	EMP2=output_singlepoint_analyse(stepoutputfile)
+	stepoutputfile=dlpno_ccsdt_singlepoint_solv(stepxyzfile)
+	EDLPNO_CCSDT=output_singlepoint_analyse(stepoutputfile)
+	
+	with open(REPORTFILE, 'a') as reportfile:
+				print(stepxyzfile.rsplit('/')[-1],geom_ok,G,G_el,EMP2,EDLPNO_CCSDT,sep=', ',file=reportfile)
---
+++ calc_pka.py
@@ -0,0 +1,79 @@
+#!/bin/python
+#Скрипт рассчёта pKa из данных о энергии молекул рассчитанных 
+#с помощью скрипта calc_dg.py и програмного пакета ORCA 5.0.3
+#На вход принимает файл csv имеющий следующий формат:
+#Имя соединения, dG рассчитанное из DFT, dG-E(el) из DFT, E(el) DLPNO-CCSD(T)
+#В файле по строкам чередуются данные для анионов и нейтральных молекул
+#Первые две строки занимает DMSO (или другой стандарт) как референс
+
+import numpy as np
+import sys
+import csv
+HEADERS=1
+T=298
+R=8.314
+PKAREF=35 #pKa стандарта
+KAREF=pow(10,-PKAREF)
+def read_inputfile(filename):
+	with open(filename, 'r') as datafile:
+		headers=datafile.readline().split(',')
+	tabl_format="U10"+",f4"*(len(headers)-1)
+	#print(tabl_format)
+	#dt=np.dtype("U10,f4,f4,f4,f4,f4")
+	dt=np.dtype(tabl_format)
+	#print(dt)
+	data=np.genfromtxt(filename, delimiter=',',dtype=dt, skip_header=HEADERS)
+	#print(len(data))
+	with open(filename, 'r') as datafile:
+		headers=datafile.readline().split(',')
+	return data,headers
+
+def calc_pka_dft_method(data,method,headers):
+	#DMSO- + acid = DMSO + anion
+	#G=[G-Eel](DFT)+Eel(DLPNO-CCSD(T))
+	#dG(A>A-)=G(A-)-G(A)
+	#dGpKa=dG(X>X-)-dG(ref>ref-)
+	#ka=KAREF*np.exp(-ddg*2.6e6/(R*T))
+	#pka=-np.log10(ka)
+	#print(">>>>>>",method,headers[method+4])
+	G=[]
+	for i in data:
+		G+=[i[3]+i[method+4]]
+	#print("G",G)	
+	dg=[]
+	i=0
+	while(i<len(data)):
+		dg+=[G[i]-G[i+1]]
+		i+=2
+	#print("dg",dg)
+	ddg=[]
+	i=0
+	while(i<len(dg)):
+		ddg+=[dg[i]-dg[0]]
+		i+=1
+	#print("ddg",ddg)
+	ddg=np.array(ddg)
+	ddg=np.float128(ddg)
+	#print(ddg)
+	ka=KAREF*np.exp(-ddg*2.6e6/(R*T))
+	#print(ka)
+	pka=-np.log10(ka)
+	#print(pka)
+	#*********REPORT*************
+	print("\npKa from DFT/",headers[method+4],sep='')
+	print("compound","Ka","pKa")
+	i=0
+	while(i<len(pka)):
+		print(data[2*i+1][0],ka[i],pka[i])
+		i+=1
+	#****************************
+inputfile=sys.argv[1]
+data, headers=read_inputfile(inputfile)
+print(headers)
+for i in range(len(headers)-4):
+	calc_pka_dft_method(data,i,headers)
+
+
+#calc_pka_dft(data)
+#calc_pka_dft_ccsdt(data)
+#print(data)
